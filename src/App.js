e2af31ea63b0c548a6452448fc703a7d319609e4
Vncent <vincentparisi@gmail.com>
2022-11-26 19:58:03 +0100

    ne ver

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Version 1.0 </h1>
        a: <input  type="text"  name="a" /> 
        b: <input  type="text"  name="b" /> 
        <br/><br/>
